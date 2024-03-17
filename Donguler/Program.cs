1033eedd61c71ec40af2821d29cdf8312fc939c2
Yagizzorlu <113940368+Yagizzorlu@users.noreply.github.com>
2024-03-17 17:00:43 +0300

    eklemeler

---
+++ Donguler/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Donguler
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Döngüsü
+            //for (int i = 0; i < 100; i++) 
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            int j;
+            for (j = 0; j < 100;j++) 
+            {
+                Console.WriteLine(j);
+            }
+            #endregion
+
+            #region While Döngüsü
+
+            for (int i = 0;i < 100;i++) 
+            {
+                Console.WriteLine("Yağız");
+            }
+
+            bool Kosul = true;
+            int Sayac = 1;
+            while(Kosul)
+            {
+                if(Sayac <= 100)
+                {
+                    Console.WriteLine("Yağız");        //Bir sayaç koyup onu arttırma işlemi yapmazsak sonsuza kadar yazdırır.
+                }
+                else
+                {
+                    Kosul = false;
+                }
+                Sayac++;
+                                 //Burada bir sayaç girdik ve 1 den başlattık. 100'e kadar artarak gitti ve kurala her uyduğunda ismimizi yazdırdı.
+                                 //100'ü aştığı zaman Kosul = false; eşitliği sağladık.
+            }
+            #endregion
+        }
+    }
+}
---
+++ KosulYapilari/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace KosulYapilari
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 5;
+            //int y = 15;
+
+            //if(x > 15)
+            //{
+            //    //Koşul doğruysa burası çalışır.
+            //} 
+            //else
+            //{
+            //   //Koşul yanlışsa burası çalışır.
+            //}
+
+            bool Yagmur = true;
+
+            if(Yagmur)
+            {
+                Console.WriteLine("Şemsiye al");    //Burayı yazdırır.
+            }
+            else
+            {
+                Console.WriteLine("Şemsiye alma");
+            }
+            //Eğer if(!Yagmur) yazarsak Şemsiye alma yazdırır. Çünkü bu Yagmur'un değili anlamına gelir ve sonucu false'dir.
+
+            //If blokları tek satırsa süslü parantez kullanmaya gerek yoktur.
+        }
+    }
+}
